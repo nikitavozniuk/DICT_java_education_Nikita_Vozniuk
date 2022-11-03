004843de469515f3e924d2586253a004553a35d1
nikitavozniuk <n.i.voznyuk@student.khai.edu>
2022-11-03 22:50:07 +0200

    ChatBot 2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,8 +1,20 @@
 package ChatBot;
 
+import java.io.*;
+
 public class ChatBot {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        InputStream inputStream = System.in;
+        Reader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
         System.out.println("Hello! My name is Carmen-Bot.");
         System.out.println("I was created in 2022.");
+
+        System.out.println("Please, remind me your name.");
+        String name = bufferedReader.readLine();
+        String output = String.format("What a great name you have, %s!", name);
+
+        System.out.println(output);
     }
 }
