27fc502edb6327d21c1e6570ba08ebb94a9b2268
nikitavozniuk <n.i.voznyuk@student.khai.edu>
2022-11-07 19:45:47 +0200

    Hangman 8-th stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -6,18 +6,88 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Hangman {
+    private static final String GAME_NAME = "HANGMAN";
+    private static final String RUNNING = "play";
+    private static final String EXIT = "exit";
+    private static final String GAME_STAGE = "Type 'play' to play the game, 'exit' to quit: > ";
+    private static final String SINGLE_LETTER = "You should input a single letter.";
+    private static final String LOWER_CASE_LETTER = "Please enter a lowercase English letter.";
+    private static final String GUESS_CONDITION = "You've already guessed this letter.";
+    private static final String LETTER_NOT_EXISTS = "That letter doesn't appear in the word.";
+
     static int attempts = 0;
     static int attemptsPossible = 8;
     static ArrayList<String> chars = new ArrayList<String>();
     static String wordToGuess = "";
     static String wordInput = "";
+    static String isRunning = RUNNING;
 
     public static void startGame() {
+        corePrep();
+        System.out.println(GAME_NAME);
+    }
+
+    public static void menu() {
+        while (!Objects.equals(isRunning, EXIT)) {
+            System.out.print(GAME_STAGE);
+            Scanner in = new Scanner(System.in);
+            isRunning = in.nextLine();
+
+            if (Objects.equals(isRunning, EXIT)) {
+                break;
+            }
+
+            core();
+            corePrep();
+        }
+    }
+
+    public static Boolean checkWin() {
+        return Objects.equals(wordInput, wordToGuess);
+    }
+
+    public static void updateWord(String word, String c) {
+        for (int index = wordToGuess.indexOf(c);
+             index >= 0;
+             index = wordToGuess.indexOf(c, index + 1))
+        {
+            word = word.substring(0, index) + c + word.substring(index + 1);
+        }
+        wordInput = word;
+    }
+
+    public static void checkInput(String input) {
+        if (input.length() > 1) {
+            System.out.println(SINGLE_LETTER);
+            return;
+        }
+        if (!input.matches("[a-zA-Z]+")) {
+            System.out.println(LOWER_CASE_LETTER);
+            return;
+        }
+
+        if (wordToGuess.contains(input) && !chars.contains(input)) {
+            updateWord(wordInput, input);
+            chars.add(input);
+        } else if (chars.contains(input)) {
+            System.out.println(GUESS_CONDITION);
+            attemptsPossible--;
+        } else {
+            System.out.println(LETTER_NOT_EXISTS);
+            attemptsPossible--;
+        }
+    }
+
+    public static void corePrep() {
+        attempts = 0;
+        attemptsPossible = 8;
+        chars = new ArrayList<String>();
+        wordToGuess = "";
+        wordInput = "";
         final String[] words = { "python", "java", "javascript", "kotlin" };
         Random random = new Random();
         int index = random.nextInt(words.length);
         wordToGuess = words[index];
-        System.out.println("HANGMAN");
     }
 
     public static void core() {
@@ -32,44 +102,18 @@ public class Hangman {
             checkInput(input);
         }
 
+        Boolean result = checkWin();
+
         System.out.println("\nThanks for playing!");
-        System.out.println("We'll see how well you did in the next stage");
-    }
-
-    public static void renderWord(String word, String c) {
-        for (int index = wordToGuess.indexOf(c);
-             index >= 0;
-             index = wordToGuess.indexOf(c, index + 1))
-        {
-            word = word.substring(0, index) + c + word.substring(index + 1);
-        }
-        wordInput = word;
-    }
-
-    public static void checkInput(String input) {
-        if (input.length() > 1) {
-            System.out.println("You should input a single letter.");
-            return;
-        }
-        if (!input.matches("[a-zA-Z]+")) {
-            System.out.println("Please enter a lowercase English letter.");
-            return;
-        }
-
-        if (wordToGuess.contains(input) && !chars.contains(input)) {
-            renderWord(wordInput, input);
-            chars.add(input);
-        } else if (chars.contains(input)) {
-            System.out.println("You've already guessed this letter.");
-            attemptsPossible--;
+        if (result) {
+            System.out.println("You win!\n");
         } else {
-            System.out.println("That letter doesn't appear in the word.");
-            attemptsPossible--;
+            System.out.println("You lose!\n");
         }
     }
 
     public static void main(String[] args) {
         startGame();
-        core();
+        menu();
     }
 }
