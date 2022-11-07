1f9c5ed7cb4e47cd9b1acbc0fd6af4fe3b7ee338
nikitavozniuk <n.i.voznyuk@student.khai.edu>
2022-11-07 20:06:50 +0200

    CoffeeMachine 2-nd stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -1,5 +1,7 @@
 package CoffeeMachine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     private static final String START_STAGE = "Starting to make a coffee";
     private static final String GRINDING_BEANS_STAGE = "Grinding coffee beans";
@@ -9,13 +11,31 @@ public class CoffeeMachine {
     private static final String POURING_MILK_STAGE = "Pouring some milk into the cup";
     private static final String READY_STAGE = "Coffee is ready!";
 
+    private static final int DEFAULT_WATER_FOR_CUP = 200;
+    private static final int DEFAULT_MILK_FOR_CUP = 50;
+    private static final int DEFAULT_BEANS_FOR_CUP = 15;
+
+    private static void result(int cups) {
+        int waterCount = DEFAULT_WATER_FOR_CUP * cups;
+        int milkCount = DEFAULT_MILK_FOR_CUP * cups;
+        int beansCount = DEFAULT_BEANS_FOR_CUP * cups;
+
+        String output = String.format(
+                "For %d cups of coffee you will need: \n%d ml of water\n%d ml of milk\n%d g of coffee beans",
+                cups,
+                waterCount,
+                milkCount,
+                beansCount
+        );
+        System.out.println(output);
+    }
+
     public static void main(String[] args) {
-        System.out.println(START_STAGE);
-        System.out.println(GRINDING_BEANS_STAGE);
-        System.out.println(BOILING_WATER_STAGE);
-        System.out.println(MIXING_STAGE);
-        System.out.println(POURING_COFFEE_STAGE);
-        System.out.println(POURING_MILK_STAGE);
-        System.out.println(READY_STAGE);
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Write how many cups of coffee you will need: ");
+        String cups = in.nextLine();
+        int parsedCups = Integer.parseInt(cups);
+        result(parsedCups);
     }
 }
